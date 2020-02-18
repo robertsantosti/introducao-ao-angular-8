b1881be269fbf5b39156412d74c855616617ea80
Robert Santos <robertsantosti@outlook.com>
2020-02-18 17:49:11 -0300

    Tratando erros

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,26 +4,21 @@ import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { Error404Component } from "./error-404/error-404.component";
 import { CourseModule } from "./courses/course.module";
 import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [AppComponent, Error404Component],
+  declarations: [AppComponent],
   imports: [
-    CoreModule,
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: "",
         redirectTo: "courses",
         pathMatch: "full"
-      },
-      {
-        path: "**",
-        component: Error404Component
       }
     ])
   ],

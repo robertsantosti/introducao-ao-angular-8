c6aaf5551df77c0f05c9663e1e37b29d2d317942
Robert Santos <robertsantosti@outlook.com>
2020-02-17 21:27:16 -0300

    Tratando eventos data binding

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
-import { CourseService } from './course.service';
+import { CourseService } from "./course.service";
 
 @Component({
   selector: "app-course-list",
   templateUrl: "./course-list.component.html"
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
 
-  constructor(private courseService: CourseService) {
-    this.courses = this.courseService.retriveAll();
+  _filterBy: string;
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this._courses = this.courseService.retriveAll();
+    this.filteredCourses = this._courses;
   }
 
-  ngOnInit(): void {}
+  set filter(value: string) {
+    this._filterBy = value;
+    this.filteredCourses = this._courses.filter(
+      (course: Course) =>
+        course.name
+          .toLocaleLowerCase()
+          .indexOf(this._filterBy.toLocaleLowerCase()) > -1
+    );
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
 }
